85e327b22e7155a5e98540de869cb85a61d3fb53
Francisco Lima Cavalcante Filho <franciscolimacf@academicos.com>
2026-06-27 14:14:24 -0300

    docker files

--- kafka-pos/notification-service/src/main/java/com/mba/notification/application/port/in/NotificationServicePort.java
+++
@@ -1,7 +0,0 @@
-package com.mba.notification.app.application.port.in;
-
-import com.mba.notification.app.application.domain.NotificationDomain;
-
-public interface NotificationServicePort {
-    NotificationDomain notify(NotificationDomain domain);
-}
--- kafka-pos/notification-service/src/main/java/com/mba/notification/application/port/out/NotificationRepositoryPort.java
+++
@@ -1,7 +0,0 @@
-package com.mba.notification.app.application.port.out;
-
-import com.mba.notification.app.application.domain.NotificationDomain;
-
-public interface NotificationRepositoryPort {
-    NotificationDomain notify(NotificationDomain domain);
-}
